a48af21eefcb0a74de556f755cfe4abe3151221a
jgraessle <j_graessle@mail.harpercollege.edu>
2024-10-27 19:47:50 -0700

    New Binary and Linear Search Methods and Method Tests added to FuzzyFinder and TestFuzzyFinder classes
    New Binary and Linear Search methods were added to the FuzzyFinder Class and tests were implemented to test whether they gave expected outcomes for the randomized and sorted lists found in the FuzzyListGenerator Class.

--- src/main/java/com/csc/FuzzyFinder.java
+++ src/main/java/com/csc/FuzzyFinder.java
@@ -1,8 +1,61 @@
 package com.csc;
 
+import java.util.ArrayList;
+
 public class FuzzyFinder {
-  // Your code goes here!
+  FuzzyListGenerator listGenerator = new FuzzyListGenerator();
+  ArrayList<Fuzzy> fuzzies = listGenerator.randomizedRainbowFuzzies();
+  String[] stringfuzzies = new String[fuzzies.size()];
+  ArrayList<Fuzzy> fuzzies2 = listGenerator.sortedRainbowFuzzies();
+  String[] stringfuzzies2 = new String[fuzzies2.size()];
+  public FuzzyFinder() {
+    for (int i = 0; i < fuzzies.size(); i++) {
+      stringfuzzies[i] = fuzzies.get(i).color;
+  }
+    for (int i = 0; i < fuzzies2.size(); i++) {
+      stringfuzzies2[i] = fuzzies2.get(i).color;
+  }
+  }
+  public int LinearFuzzySearch(String[] numbers, String numberToFind) {
+    for (int i = 0; i < numbers.length; i++) {
+      if(numbers[i].equals(numberToFind)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  public int BinaryFuzzySearch(String[] numbers, String numberToFind) {
+    int low = 0;
+    int high = numbers.length - 1;
+
+    while (low <= high) {
+      int middlePosition = (low + high) / 2;
+      String middleNumber = numbers[middlePosition];
+      int compare = numberToFind.compareTo(middleNumber);
+      if (numberToFind.equals(middleNumber)) {
+        return middlePosition;
+      }
+      if (compare < 0) {
+        high = middlePosition - 1;
+      }
+      else {
+        low = middlePosition + 1;
+      }
+    }
+    return -1;
+  }
+  
   public static void main(String args[]) {
     System.out.println("In addition to your search functions, you can write a main function if you wish!");
+    FuzzyFinder fuzzyFinder = new FuzzyFinder();
+    int output = fuzzyFinder.LinearFuzzySearch(fuzzyFinder.stringfuzzies, "gold");
+    System.out.println(output);
+    int output2 = fuzzyFinder.BinaryFuzzySearch(fuzzyFinder.stringfuzzies, "gold");
+    System.out.println(output2);
+    int output3 = fuzzyFinder.LinearFuzzySearch(fuzzyFinder.stringfuzzies2, "gold");
+    System.out.println(output3);
+    int output4 = fuzzyFinder.BinaryFuzzySearch(fuzzyFinder.stringfuzzies2, "gold");
+    System.out.println(output4);
+    
   }
 }
--- src/test/java/com/csc/TestFuzzyFinder.java
+++ src/test/java/com/csc/TestFuzzyFinder.java
@@ -17,16 +17,26 @@ public class TestFuzzyFinder {
     finder = new FuzzyFinder();
     generator = new FuzzyListGenerator();
   }
-
   @Test
-  void exampleFailingTestWithRandomizedFuzzies() {
+  void RandomLinearTest() {
     ArrayList<Fuzzy> fuzzies = generator.randomizedRainbowFuzzies();
-    assertEquals("purple", fuzzies.getFirst().color);
+    assertEquals(3500, finder.LinearFuzzySearch(finder.stringfuzzies, "gold"), 3500);
+  }
+  @Test
+  void RandomBinaryTest() {
+    ArrayList<Fuzzy> fuzzies = generator.randomizedRainbowFuzzies();
+    assertEquals(-1, finder.BinaryFuzzySearch(finder.stringfuzzies, "gold"));
+  }
+  @Test
+  void SortedLinearTest() {
+    ArrayList<Fuzzy> fuzzies = generator.randomizedRainbowFuzzies();
+    assertEquals(1000, finder.LinearFuzzySearch(finder.stringfuzzies2, "gold"));
+  }
+  @Test
+  void SortedBinaryTest() {
+    ArrayList<Fuzzy> fuzzies = generator.randomizedRainbowFuzzies();
+    assertEquals(1000, finder.BinaryFuzzySearch(finder.stringfuzzies2, "gold"));
   }
 
-  @Test
-  void exampleFailingTestWithSortedFuzzies() {
-    ArrayList<Fuzzy> fuzzies = generator.sortedRainbowFuzzies();
-    assertEquals("purple", fuzzies.getFirst().color);
-  }
+ 
 }
